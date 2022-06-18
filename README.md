326b13a845fa2641f84c831ef3c68b795f7443d8
Triston Armstrong <triston95strong@gmail.com>
2022-06-18 16:56:43 -0400

    More config attempts

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+[![Code Style: Google](https://img.shields.io/badge/code%20style-google-blueviolet.svg)](https://github.com/google/gts)
+
 # component-test-helper
 The Component Test Helper is a package intended to make React Jest testing far more declaritive
---
+++ src/types/ComponentTestHelper.d.ts
@@ -0,0 +1,69 @@
+import {
+  ByRoleMatcher,
+  ByRoleOptions,
+  Matcher,
+  SelectorMatcherOptions,
+  MatcherOptions,
+} from '@testing-library/react';
+
+export type QueryExecutionType =
+  | 'get'
+  | 'getAll'
+  | 'find'
+  | 'findAll'
+  | 'query'
+  | 'queryAll';
+
+export type RoleQueryType = 'ByRole';
+
+export interface ByRoleSelection {
+  readonly query: RoleQueryType;
+  readonly id: ByRoleMatcher;
+  readonly options?: ByRoleOptions;
+}
+
+export type TextSelectionQueryType = 'ByText' | 'ByLabelText';
+
+export interface ByTextSelection {
+  readonly query: TextSelectionQueryType;
+  readonly id: Matcher;
+  readonly options?: SelectorMatcherOptions;
+}
+
+export type BoundAttribute =
+  | 'LabelText'
+  | 'AltText'
+  | 'DisplayValue'
+  | 'TestId'
+  | 'PlaceholderText'
+  | 'Title';
+
+export type BoundAttributeQueryType = `By${BoundAttribute}`;
+
+export interface ByBoundAttributeSelection {
+  readonly query: BoundAttributeQueryType;
+  readonly id: Matcher;
+  readonly options?: MatcherOptions;
+}
+
+export type Selection =
+  | ByRoleSelection
+  | ByTextSelection
+  | ByBoundAttributeSelection;
+
+export interface TypeOptions {
+  realistic?: boolean;
+}
+
+export type SelectionMap<
+  TRootElement extends HTMLElement = HTMLElement,
+  TSelectedElement extends HTMLElement = TRootElement
+> = (el: TSelectedElement | null) => TRootElement | null;
+
+export type ContainerGetter = () => HTMLElement | Promise<HTMLElement>;
+
+export type Selection = Selection & {
+  id: string;
+  options: Record<string, string>;
+  query: string;
+};
