c50c1de898782de231ecd6bb760d14f1f07d3bbb
Triston Armstrong <triston95strong@gmail.com>
2022-07-04 10:18:48 -0500

    chore: improve type annotations
    Also updated commitizen to use npx instead of git which requires developers to install commitizen
    globally

--- src/lib/ComponentTestHelper.ts
+++ src/lib/ComponentTestHelper.ts
@@ -1,4 +1,4 @@
-import { screen, within } from './testUtils';
+import { screen, within } from './testUtils'
 
 import {
   ByRoleMatcher,
@@ -7,19 +7,19 @@ import {
   Matcher,
   MatcherOptions,
   SelectorMatcherOptions,
-  waitForOptions,
-} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+  waitForOptions
+} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import {
   TypeOptions,
   QueryExecutionType,
   BoundAttribute,
   ByBoundAttributeSelection,
   SelectionMap,
-  ContainerGetter,
-} from '../types/ComponentTestHelper';
+  ContainerGetter
+} from '../types/ComponentTestHelper'
 
-import { Selection } from '../types/ComponentTestHelper';
+import { Selection } from '../types/ComponentTestHelper'
 
 /**
  * @class
@@ -27,8 +27,8 @@ import { Selection } from '../types/ComponentTestHelper';
 export class ComponentTestHelper<
   TRootElement extends HTMLElement = HTMLElement,
   TSelectedElement extends HTMLElement = TRootElement
-  > {
-  protected readonly selector: Selector<TRootElement, TSelectedElement>;
+> {
+  protected readonly selector: Selector<TRootElement, TSelectedElement>
 
   /**
    * @param {void} configureSelector the selector to configure builder context
@@ -40,254 +40,223 @@ export class ComponentTestHelper<
   ) {
     this.selector = configureSelector(
       Selector.createBuilder<TRootElement, TSelectedElement>()
-    ).build();
+    ).build()
   }
 
-  /**
-   * @return {promise} gets the html element
-   */
-  async get() {
-    return await this.selector.get();
+  async get(): Promise<TRootElement> {
+    return await this.selector.get()
   }
 
-  /**
-   * @return {promise} gets all elements
-   */
-  async getAll() {
-    return await this.selector.getAll();
+  async getAll(): Promise<TRootElement[]> {
+    return await this.selector.getAll()
   }
 
-  /**
-   * @return {promise} query all elements
-   */
-  async query() {
-    return await this.selector.query();
+  async query(): Promise<TRootElement | null> {
+    return await this.selector.query()
   }
 
-  /**
-   * @return {promise} query all elements
-   */
-  async queryAll() {
-    return await this.selector.queryAll();
+  async queryAll(): Promise<TRootElement[]> {
+    return await this.selector.queryAll()
   }
 
-  /**
-   * @return {promise} find this element
-   */
-  async find() {
-    return await this.selector.find();
+  async find(): Promise<TRootElement> {
+    return await this.selector.find()
   }
 
-  /**
-   * @return {promise} find all elements
-   */
-  async findAll(): Promise<unknown> {
-    return await this.selector.findAll();
+  async findAll(): Promise<TRootElement[]> {
+    return await this.selector.findAll()
   }
 
-  /** clears elements value */
-  async clear() {
-    await userEvent.clear(await this.get());
+  /** Clears the value of this element */
+  async clear(): Promise<void> {
+    await userEvent.clear(await this.get())
   }
 
   /**
    * @param {string} value the value to type in the field
-   * @param {object} options Possible options to change the typeing behavior
-   *  currently only supporting property "realistic"
-   * @return {void} void
+   * @param {object} options Possible options to change the typeing behavior - current supported (`realistic`)
    */
-  async type(value: string, options?: TypeOptions) {
-    if (!value) return;
+  async type(value: string, options?: TypeOptions): Promise<void> {
+    if (!value) return
 
-    const el = await this.get();
+    const el = await this.get()
     if (options?.realistic) {
-      await userEvent.type(el, value);
-    } else {
-      await userEvent.click(el);
-      fireEvent.change(el, {
-        target: {
-          name: (el as unknown as HTMLInputElement).name,
-          value: (el as unknown as HTMLInputElement).value + value,
-        },
-      });
+      await userEvent.type(el, value)
+      return
     }
+
+    await userEvent.click(el)
+    fireEvent.change(el, {
+      target: {
+        name: (el as unknown as HTMLInputElement).name,
+        value: (el as unknown as HTMLInputElement).value + value
+      }
+    })
   }
 
-  /**
-   * Clicks this element
-   */
-  async click() {
-    await userEvent.click(await this.get());
+  async click(): Promise<void> {
+    await userEvent.click(await this.get())
   }
 
-  /**
-   * finds this element and jest expects it to be in the document
-   */
-  async expectToBeInTheDocument() {
-    expect(await this.find()).toBeInTheDocument();
+  async expectToBeInTheDocument(): Promise<void> {
+    expect(await this.find()).toBeInTheDocument()
   }
 
-  /**
-   * searches for this element and jest expects it to not be in the document
-   */
-  async expectNotToBeInTheDocument() {
-    expect(await this.query()).not.toBeInTheDocument();
+  async expectNotToBeInTheDocument(): Promise<void> {
+    expect(await this.query()).not.toBeInTheDocument()
   }
 }
 
-/**
- * @class
- */
 export class Selector<
   TRootElement extends HTMLElement = HTMLElement,
   TSelectedElement extends HTMLElement = TRootElement
-  > {
-  readonly selection: Selection;
-  #getContainer?: ContainerGetter;
-  #map?: SelectionMap<TRootElement, TSelectedElement>;
+> {
+  readonly selection: Selection
+  #getContainer?: ContainerGetter
+  #map?: SelectionMap<TRootElement, TSelectedElement>
 
   constructor(
     selection: Selection,
     getContainer?: ContainerGetter,
     map?: SelectionMap<TRootElement, TSelectedElement>
   ) {
-    this.selection = selection;
-    this.#getContainer = getContainer;
-    this.#map = map;
+    this.selection = selection
+    this.#getContainer = getContainer
+    this.#map = map
   }
 
   static createBuilder<
     TRootElement extends HTMLElement = HTMLElement,
     TSelectedElement extends HTMLElement = TRootElement
   >() {
-    return new SelectorBuilder<TRootElement, TSelectedElement>();
+    return new SelectorBuilder<TRootElement, TSelectedElement>()
   }
 
-  async get() {
-    return (await this.execQuery('get')) as TRootElement;
+  async get(): Promise<TRootElement> {
+    return (await this.execQuery('get')) as TRootElement
   }
 
-  async getAll() {
-    return (await this.execQuery('getAll')) as TRootElement[];
+  async getAll(): Promise<TRootElement[]> {
+    return (await this.execQuery('getAll')) as TRootElement[]
   }
 
-  async find(waitForOptions?: waitForOptions) {
-    return (await this.execQuery('find', waitForOptions)) as TRootElement;
+  async find(waitForOptions?: waitForOptions): Promise<TRootElement> {
+    return (await this.execQuery('find', waitForOptions)) as TRootElement
   }
 
-  async findAll(waitForOptions?: waitForOptions) {
-    return (await this.execQuery('findAll', waitForOptions)) as TRootElement[];
+  async findAll(waitForOptions?: waitForOptions): Promise<TRootElement[]> {
+    return (await this.execQuery('findAll', waitForOptions)) as TRootElement[]
   }
 
-  async query() {
-    return (await this.execQuery('query')) as TRootElement | null;
+  async query(): Promise<TRootElement | null> {
+    return (await this.execQuery('query')) as TRootElement | null
   }
 
-  async queryAll() {
-    return (await this.execQuery('queryAll')) as TRootElement[];
+  async queryAll(): Promise<TRootElement[]> {
+    return (await this.execQuery('queryAll')) as TRootElement[]
   }
 
   private async execQuery(
     type: QueryExecutionType,
     waitForOptions?: waitForOptions
   ): Promise<TRootElement[] | TRootElement | null> {
-    const methodName = `${type}${this.selection.query}`;
-    const args = [this.selection.id, this.selection.options, waitForOptions];
-
+    const methodName = `${type}${this.selection.query}`
+    const args = [this.selection.id, this.selection.options, waitForOptions]
     const scopedQueries = this.#getContainer
       ? within(await this.#getContainer())
-      : screen;
+      : screen
+
     // there is no good way to properly type this
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
     const result = (await scopedQueries[methodName](...args)) as
       | TSelectedElement[]
       | TSelectedElement
-      | null;
+      | null
 
-    if (this.#map) {
-      return Array.isArray(result)
-        ? (result.map(this.#map) as TRootElement[])
-        : this.#map(result);
-    } else {
-      return result as TRootElement[] | TRootElement | null;
+    if (!this.#map) {
+      return result as TRootElement[] | TRootElement | null
     }
+
+    return Array.isArray(result)
+      ? (result.map(this.#map) as TRootElement[])
+      : this.#map(result)
   }
 }
 
 export class SelectorBuilder<
   TRootElement extends HTMLElement = HTMLElement,
   TSelectedElement extends HTMLElement = TRootElement
-  > {
-  #selection?: Selection;
-  #getContainer?: ContainerGetter;
-  #map?: SelectionMap<TRootElement, TSelectedElement>;
+> {
+  #selection?: Selection
+  #getContainer?: ContainerGetter
+  #map?: SelectionMap<TRootElement, TSelectedElement>
 
   byRole(role: ByRoleMatcher, options?: ByRoleOptions): this {
-    this.#selection = { query: 'ByRole', id: role, options };
-    return this;
+    this.#selection = { query: 'ByRole', id: role, options }
+    return this
   }
 
   byText(id: Matcher, options?: SelectorMatcherOptions): this {
-    this.#selection = { query: 'ByText', id, options };
-    return this;
+    this.#selection = { query: 'ByText', id, options }
+    return this
   }
 
   byLabelText(id: Matcher, options?: SelectorMatcherOptions): this {
-    this.#selection = { query: 'ByLabelText', id, options };
-    return this;
+    this.#selection = { query: 'ByLabelText', id, options }
+    return this
   }
 
   byAltText(id: Matcher, options?: MatcherOptions): this {
-    this.#selection = this.byBoundAttribute('AltText', id, options);
-    return this;
+    this.#selection = this.byBoundAttribute('AltText', id, options)
+    return this
   }
 
   byDisplayValue(id: Matcher, options?: MatcherOptions): this {
-    this.#selection = this.byBoundAttribute('AltText', id, options);
-    return this;
+    this.#selection = this.byBoundAttribute('AltText', id, options)
+    return this
   }
 
   byTestId(id: Matcher, options?: MatcherOptions): this {
-    this.#selection = this.byBoundAttribute('TestId', id, options);
-    return this;
+    this.#selection = this.byBoundAttribute('TestId', id, options)
+    return this
   }
 
   byPlaceholderText(id: Matcher, options?: MatcherOptions): this {
-    this.#selection = this.byBoundAttribute('PlaceholderText', id, options);
-    return this;
+    this.#selection = this.byBoundAttribute('PlaceholderText', id, options)
+    return this
   }
 
   byTitle(id: Matcher, options?: MatcherOptions): this {
-    this.#selection = this.byBoundAttribute('Title', id, options);
-    return this;
+    this.#selection = this.byBoundAttribute('Title', id, options)
+    return this
   }
 
   withMapping(map: SelectionMap<TRootElement, TSelectedElement>): this {
-    this.#map = map;
-    return this;
+    this.#map = map
+    return this
   }
 
   withinContainer(container: ContainerGetter | ComponentTestHelper): this {
     this.#getContainer =
       typeof container === 'function'
         ? container
-        : async () => await container.get();
-    return this;
+        : async () => await container.get()
+    return this
   }
 
   build(): Selector<TRootElement, TSelectedElement> {
     if (!this.#selection) {
       throw new Error(
         'Must have at least one selection when building a selector (e.g. builder.byRole(...))'
-      );
+      )
     }
 
     return new Selector<TRootElement, TSelectedElement>(
       this.#selection,
       this.#getContainer,
       this.#map
-    );
+    )
   }
 
   private byBoundAttribute(
@@ -298,7 +267,7 @@ export class SelectorBuilder<
     return {
       query: `By${attribute}`,
       id,
-      options,
-    };
+      options
+    }
   }
 }
